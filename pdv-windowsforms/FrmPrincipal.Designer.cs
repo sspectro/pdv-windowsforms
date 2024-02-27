dfcd2c9ff3f1dfa5dc30f35edd07925fc14ee939
sspectro <cristtiano.mendonca@gmail.com>
2024-02-27 11:09:13 -0300

    Inclusão de MenuStrip ao FormPrincipal - Cadastros, Produtos, Movimentações, Relatórios e Sair

---
+++ pdv-windowsforms/FrmPrincipal.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace pdv_windowsforms
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MenuCadastros = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuProdutos = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuMovimentacoes = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuRelatorios = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuSairPrincipal = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuPrincipal = new System.Windows.Forms.MenuStrip();
+            this.img03 = new System.Windows.Forms.PictureBox();
+            this.img04 = new System.Windows.Forms.PictureBox();
+            this.img02 = new System.Windows.Forms.PictureBox();
+            this.img01 = new System.Windows.Forms.PictureBox();
+            this.MenuPrincipal.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.img03)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img04)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img02)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img01)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MenuCadastros
+            // 
+            this.MenuCadastros.Name = "MenuCadastros";
+            this.MenuCadastros.Size = new System.Drawing.Size(71, 20);
+            this.MenuCadastros.Text = "Cadastros";
+            // 
+            // MenuProdutos
+            // 
+            this.MenuProdutos.Name = "MenuProdutos";
+            this.MenuProdutos.Size = new System.Drawing.Size(67, 20);
+            this.MenuProdutos.Text = "Produtos";
+            // 
+            // MenuMovimentacoes
+            // 
+            this.MenuMovimentacoes.Name = "MenuMovimentacoes";
+            this.MenuMovimentacoes.Size = new System.Drawing.Size(104, 20);
+            this.MenuMovimentacoes.Text = "Movimentações";
+            // 
+            // MenuRelatorios
+            // 
+            this.MenuRelatorios.Name = "MenuRelatorios";
+            this.MenuRelatorios.Size = new System.Drawing.Size(71, 20);
+            this.MenuRelatorios.Text = "Relatórios";
+            // 
+            // MenuSairPrincipal
+            // 
+            this.MenuSairPrincipal.Name = "MenuSairPrincipal";
+            this.MenuSairPrincipal.Size = new System.Drawing.Size(38, 20);
+            this.MenuSairPrincipal.Text = "Sair";
+            // 
+            // MenuPrincipal
+            // 
+            this.MenuPrincipal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuCadastros,
+            this.MenuProdutos,
+            this.MenuMovimentacoes,
+            this.MenuRelatorios,
+            this.MenuSairPrincipal});
+            this.MenuPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.MenuPrincipal.Name = "MenuPrincipal";
+            this.MenuPrincipal.Size = new System.Drawing.Size(1037, 24);
+            this.MenuPrincipal.TabIndex = 0;
+            this.MenuPrincipal.Text = "menuStrip1";
+            // 
+            // img03
+            // 
+            this.img03.Location = new System.Drawing.Point(209, 274);
+            this.img03.Name = "img03";
+            this.img03.Size = new System.Drawing.Size(278, 215);
+            this.img03.TabIndex = 2;
+            this.img03.TabStop = false;
+            // 
+            // img04
+            // 
+            this.img04.Location = new System.Drawing.Point(662, 274);
+            this.img04.Name = "img04";
+            this.img04.Size = new System.Drawing.Size(278, 215);
+            this.img04.TabIndex = 3;
+            this.img04.TabStop = false;
+            // 
+            // img02
+            // 
+            this.img02.Location = new System.Drawing.Point(662, 36);
+            this.img02.Name = "img02";
+            this.img02.Size = new System.Drawing.Size(278, 215);
+            this.img02.TabIndex = 4;
+            this.img02.TabStop = false;
+            // 
+            // img01
+            // 
+            this.img01.Location = new System.Drawing.Point(209, 36);
+            this.img01.Name = "img01";
+            this.img01.Size = new System.Drawing.Size(278, 215);
+            this.img01.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.img01.TabIndex = 5;
+            this.img01.TabStop = false;
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(1037, 536);
+            this.Controls.Add(this.img01);
+            this.Controls.Add(this.img02);
+            this.Controls.Add(this.img04);
+            this.Controls.Add(this.img03);
+            this.Controls.Add(this.MenuPrincipal);
+            this.MainMenuStrip = this.MenuPrincipal;
+            this.Name = "frmPrincipal";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Principal";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.MenuPrincipal.ResumeLayout(false);
+            this.MenuPrincipal.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.img03)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img04)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img02)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img01)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolStripMenuItem MenuCadastros;
+        private System.Windows.Forms.ToolStripMenuItem MenuProdutos;
+        private System.Windows.Forms.ToolStripMenuItem MenuMovimentacoes;
+        private System.Windows.Forms.ToolStripMenuItem MenuRelatorios;
+        private System.Windows.Forms.ToolStripMenuItem MenuSairPrincipal;
+        private System.Windows.Forms.MenuStrip MenuPrincipal;
+        private System.Windows.Forms.PictureBox img03;
+        private System.Windows.Forms.PictureBox img04;
+        private System.Windows.Forms.PictureBox img02;
+        private System.Windows.Forms.PictureBox img01;
+    }
+}
+
